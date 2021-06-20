b756814d937a5348add6ce16d325aab6f59d7926
Gustavo <gustavoleone3456@hotmail.com>
2021-06-20 01:23:57 -0300

    classe page criada

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use  \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+$page = new Page();
 
-	echo json_encode($results);
+$page->setTpl("index");
 
 });
 
