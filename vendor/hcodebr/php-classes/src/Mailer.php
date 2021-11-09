533c066b6cca5588f7da86ee33ba9c52bb022f8e
Gustavo Arruda <70242475+gugalxp@users.noreply.github.com>
2021-11-09 16:57:03 -0300

    Update Mailer.php

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -6,8 +6,8 @@ use Rain\Tpl;
 
 class Mailer {
 	
-	const USERNAME = "curttorock@gmail.com";
-	const PASSWORD = "gugalxp500";
+	const USERNAME = "";
+	const PASSWORD = "";
 	const NAME_FROM = "Hcode Store";
 
 	private $mail;
